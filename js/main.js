8d9c023fc32de9aea1e78c7764d4377a1a1aeaa6
Paul Yuan <paul.yuan@ubitus.net>
2014-09-04 12:15:14 +0800

    added lazy load support

--- js/main.js
+++ js/main.js
@@ -1,4 +1,56 @@
+function checkVisible(){
 
+  var galleries=$(".galleria:in-viewport");
+
+  var gallery;
+  for (var i=0; i< galleries.length; i++){
+    gallery=$(galleries[i]);
+    if(!gallery.hasClass("justified-gallery")){
+      createGallery(gallery);
+      break;
+    }
+  }
+
+
+
+
+}
+function createGallery(gallery){
+  var group=gallery.attr("id");
+
+  if(gallery.hasClass("justified-gallery"))
+    return;
+
+  console.log("creating gallery", group)
+
+  for (var i=0; i< gallery_images.length; i++){
+
+    var item=gallery_images[i];
+
+    if(item.group!=group)
+       continue;
+
+    var link=$("<a>").attr({href: item.src, title:item.CreateDate, class: "swipebox"})
+      var thumb=$("<img>").attr({"src": item.thumb});
+    link.append(thumb);
+    gallery.append(link);
+
+  }
+
+  gallery.justifiedGallery({
+    sizeRangeSuffixes:{'lt100':'_t', 
+      'lt240':'_t', 
+    'lt320':'_t', 
+    'lt500':'', 
+    'lt640':'_t', 
+    'lt1024':'_t'} 
+  }).on('jg.resize', function(e){
+    
+    checkVisible();
+    
+    
+  });
+}
 
 
 
@@ -11,35 +63,25 @@ $(function(){
 
     // create new week 
     if(item.group!=group){
-      // enable gallery
-      if(gallery!=""){
-        gallery.justifiedGallery({
-          sizeRangeSuffixes:{'lt100':'_t', 
-            'lt240':'_t', 
-          'lt320':'_t', 
-          'lt500':'', 
-          'lt640':'_t', 
-          'lt1024':'_t'}
-        });
-
-      }
       var header=$("<h3>").html(item.group)
-        group=item.group
+      group=item.group
 
-        gallery=$("<div>")
-        $("#container").append(header);
+      gallery=$("<div>").attr({id: item.group, class: "galleria"});
+      $("#container").append(header);
       $("#container").append(gallery);
 
     }
 
-    var link=$("<a>").attr({href: item.src, title:item.CreateDate, class: "swipebox"})
-      var thumb=$("<img>").attr({"src": item.thumb});
-    link.append(thumb);
-    gallery.append(link);
 
   }
+
+checkVisible();
 
 
 $(".swipebox").swipebox();
 
 })
+
+$(window).scroll(checkVisible)
+  
+  
