a2567bf98878b313dc53f4beb73732cc041a9b2c
Paul Yuan <paul.yuan@ubitus.net>
2016-05-09 16:37:52 +0800

    rename files

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.MD') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='photo-dedup',
+    version='0.1',
+    description='A photo deduplicator based on Exif meta data',
+    long_description=readme,
+    author='Paul Yuan',
+    author_email='puyuan1@gmail.com',
+    url='https://github.com/puyuan',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
