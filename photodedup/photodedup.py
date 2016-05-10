8bcf64ccaa2d2ef6bf7134ebf2450089f8fd5ea1
Paul Yuan <puyuan1@gmail.com>
2016-05-11 00:25:03 +0800

    update dedup

--- photodedup/photodedup.py
+++ photodedup/photodedup.py
@@ -14,7 +14,7 @@ logger.debug('often makes a very good meal of %s', 'visiting tourists')
 
 
 def get_metadata_batch():
-    tags=["ImageWidth", "ImageHeight", "SourceFile","CreateDate", "GPSLatitude", "GPSLongitude", "GPSAltitude"]
+    tags=["ImageWidth", "ImageHeight", "SourceFile","CreateDate", "DateAcquired", "GPSLatitude", "GPSLongitude", "GPSAltitude"]
     with exiftool.ExifTool() as et:
         for root, dirs, files in os.walk("/mnt/hgfs/pictures"):
             fileList = [join(root, f) for f in files if ".jpg" in f.lower()]
@@ -25,15 +25,17 @@ def get_metadata_batch():
 
 
 class PhotoIndex():
-    def __init__(self):
+    def __init__(self, image_folder_path):
         self.db=sqlite3.connect("images.sqlite")
+        self.image_folder_path=image_folder_path
 
     def insert_index(self):
         c = self.db.cursor()
         for metadataList in get_metadata_batch():
 
 
-            columns= [(d.get("EXIF:CreateDate", ""),
+            columns= [(d.get("EXIF:CreateDate", d.get("DateAcquired","")),
+                        d.get("EXIF:CreateDate", ""),
                        d.get("EXIF:GPSLatitude", ""),
                        d.get("EXIF:GPSLongitude", ""),
                        d.get("EXIF:GPSAltitude", ""),
@@ -45,7 +47,7 @@ class PhotoIndex():
             for metadata in metadataList:
                 logger.info("inserting %s", metadata.get('SourceFile', ""))
 
-            c.executemany("insert or ignore into images values (?, ?, ? ,?, ?)", columns)
+            c.executemany("insert or ignore into images values (?, ?, ? ,?, ?, ?)", columns)
             self.db.commit()
 
 
@@ -57,13 +59,45 @@ class PhotoIndex():
     def create_index(self):
         c = self.db.cursor()
         c.execute('''create table if not exists images
-                 (CreateDate text primary key,
+                 (timestamp text primary key,
+                 CreateDate text,
               GPSLatitude real,
         GPSLongitude   real,
             GPSAltitude    real ,
             SourceFile   text
                   )''')
 
-photoIndex=PhotoIndex()
+    def dumpDB(self):
+        logger.info("Get all images in index")
+        c=self.db.cursor()
+        file=open("/tmp/sqlite_files.log", "w")
+        for (sourceFile,) in c.execute("select SourceFile from images order by SourceFile"):
+            file.write(sourceFile+"\n")
+        file.close()
+
+    def dumpImageFolder(self):
+        logger.info("Get all images in image folder")
+        outfile = open("/tmp/imagefolder_files.log", "w")
+        for root, dirs, files in os.walk(self.image_folder_path):
+            dirs.sort()
+            files.sort()
+            for file in files:
+                if ".jpg" in file.lower():
+                    fullpath=join(root, file)
+                    outfile.write(fullpath + "\n")
+        outfile.close()
+
+    def fetchNewImage(self):
+        logger.info("Fetching new images")
+        with open('/tmp/imagefolder_files.log')  as f, open('/tmp/sqlite_files.log') as f2:
+            lines1 = set(map(str.rstrip, f))
+            print(lines1.difference(map(str.rstrip, f2)))
+
+
+
+photoIndex=PhotoIndex("/mnt/hgfs/pictures")
 photoIndex.create_index()
+photoIndex.dumpDB()
+photoIndex.dumpImageFolder()
+photoIndex.fetchNewImage()
 photoIndex.insert_index()
