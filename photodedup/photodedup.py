3301abef0b0aa96b01cac142839423033028221d
Paul Yuan <paul.yuan@ubitus.net>
2016-05-16 18:25:48 +0800

    clean up

--- photodedup/photodedup.py
+++ photodedup/photodedup.py
@@ -66,73 +66,6 @@ class PhotoDedup():
             cur.executemany("delete from images where SourceFile = ? ", columns)
             self.conn.commit()
 
-    def get_new_images(self):
-        logger.info("Fetching new images")
-        dbImages=self.__get_db_images()
-        folderImages=self.__get_folder_images()
-
-        # use sorted iterator to find new images
-        done = object()
-        dbNext = next(dbImages, done)
-        folderNext = next(folderImages, done)
-
-        while (dbNext is not done) and (folderNext is not done):
-          logger.debug("dbNext: %s, folderNext %s"%(dbNext, folderNext))
-          if (dbNext > folderNext):
-              yield folderNext.rstrip()
-              folderNext = next(folderImages, done)
-          elif (dbNext < folderNext):
-              dbNext=next(dbImages, done)
-          else: # equals
-              folderNext = next(folderImages, done)
-              dbNext = next(dbImages, done)
-
-        while folderNext is not done:
-            logger.debug("dbNext: %s, folderNext %s" % (dbNext, folderNext))
-            yield folderNext.rstrip()
-            folderNext = next(folderImages, done)
-
-
-
-    def check_new_images(self):
-        logger.info("checking new images")
-
-        fileList=[image for image in self.get_new_images()]
-        if not fileList:
-            logger.info("list is empty")
-            return False
-
-        logger.info("List is not empty, following files are left:")
-        for file in fileList:
-            logger.info(file)
-        return True
-
-    def print_new_images_count(self):
-        logger.info("Estimated new images: %d", self.get_new_images_count())
-
-    def get_new_images_count(self):
-        logger.info("checking new images")
-
-        count=0
-        for image in self.get_new_images():
-            logger.debug("Counting image: %s"% image)
-            count+=1
-        return count
-
-
-
-
-
-#    def remove_images(self, duplicate_images):
-#      for file in duplicate_images:
-#          os.remove(file)
-
-    def copy_duplicates(self):
-      self.insert_new_images()
-      count=0
-      for file in self.get_new_images():
-          count+=1
-          shutil.copy(file, '/tmp/test/%d.jpg'%(count))
 
 
     def __get_images_metadata(self, images):
@@ -142,26 +75,6 @@ class PhotoDedup():
             tags["SourceFile"]=filename
             yield tags
             f.close()
-
-    def __get_db_images(self):
-        logger.info("Get all images in index")
-        c = self.conn.cursor()
-
-        for (sourceFile,) in c.execute("select SourceFile from images order by SourceFile"):
-            yield sourceFile.encode('utf-8')
-
-
-
-    def __get_folder_images(self):
-        logger.info("Get all images in image folder")
-        for root, dirs, files in os.walk(self.image_folder_path):
-            dirs.sort()
-            files.sort()
-            for file in files:
-
-                if ".jpg" in file.lower():
-                    fullpath=join(root, file)
-                    yield fullpath
 
 def split_every(n, iterable):
     i = iter(iterable)
